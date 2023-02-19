23ab5a4a6edbb4a6241d5a8f30cf09b9c88cb0d8
Élio Vieira <eliovieiraa@gmail.com>
2023-02-19 19:48:38 +0000

    hackerrank - test - javascript

---
+++ Notes-Store-Class.js
@@ -0,0 +1,33 @@
+class NotesStore {
+  constructor(name, state) {
+    this.notes = [];
+  }
+
+  addNote(state, name) {
+    if (!name) {
+      throw new Error("Error: Name cannot be empty");
+    } else if (
+      state !== "completed" &&
+      state !== "active" &&
+      state !== "others"
+    ) {
+      throw new Error(`Error: Invalid state " + ${state}`);
+    } else {
+      this.notes.push({ name: name, state: state });
+    }
+  }
+
+  getNotes(state) {
+    const arr = [];
+    if (state !== "completed" && state !== "active" && state !== "others") {
+      throw new Error(`Error: Invalid state " + ${state}`);
+    } else {
+      for (let i = 0; i < this.notes.length; i++) {
+        if (this.notes[i].state === state) {
+          arr.push(this.notes[i].name);
+        }
+      }
+    }
+    return arr;
+  }
+}
